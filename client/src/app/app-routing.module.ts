0cc907a24d1c58289a60be6287ee5df9cc5bdebc
Michael Lauritsen <i30288741@gmail.com>
2021-02-28 14:32:17 +0100

    added routeGuard

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { ListsComponent } from './lists/lists.component';
 import { MemberDetailComponent } from './members/member-detail/member-detail.component';
 import { MemberListComponent } from './members/member-list/member-list.component';
 import { MessagesComponent } from './messages/messages.component';
+import { AuthGuard } from './_guards/auth.guard';
 
 const routes: Routes = [
   {
@@ -14,6 +15,7 @@ const routes: Routes = [
   {
     path: 'members',
     component: MemberListComponent,
+    canActivate: [AuthGuard],
   },
   {
     path: 'members/:id',
