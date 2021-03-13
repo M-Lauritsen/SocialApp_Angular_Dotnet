7699e690d346f43fd8cc5ac1fd9939ec7a80dbc8
Michael Lauritsen <i30288741@gmail.com>
2021-03-13 06:51:44 +0100

    done with automapper

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -28,20 +28,16 @@ namespace API.Controllers
         [HttpGet]
         public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers()
         {
-            var users = await _userRepo.GetUsersAsync();
+            var users = await _userRepo.GetMembersAsync();
 
-            var usersToReturn = _mapper.Map<IEnumerable<MemberDto>>(users);
-
-            return Ok(usersToReturn);
+            return Ok(users);
         }
 
 
         [HttpGet("{username}")]
         public async Task<ActionResult<MemberDto>> GetUser(string username)
         {
-            var user = await _userRepo.GetUserByUsernameAsync(username);
-            
-            return _mapper.Map<MemberDto>(user);
+            return await _userRepo.GetMemberAsync(username);
         }
     }
 }
