a294ae1c670ccc06e2ab888bb6136d0796fe8836
Michael Lauritsen <mla@complyto.com>
2021-04-15 07:07:28 +0200

    message repo & automapper

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -25,7 +25,6 @@ namespace API.Entities
         public ICollection<UserLike> LikedByUsers { get; set; }
 
         public ICollection<UserLike> LikedUsers { get; set; }
-''
         public ICollection<Message> MessageSent { get; set; }
         public ICollection<Message> MessageReceived { get; set; }
 
