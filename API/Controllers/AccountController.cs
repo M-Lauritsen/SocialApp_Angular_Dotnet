9e5e4963055d05d6783e271ad4269a17c4707252
Michael Lauritsen <mla@complyto.com>
2021-05-04 07:18:37 +0200

    added user + signinMananger

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -18,8 +18,10 @@ namespace API.Controllers
         private readonly ITokenService _tokenService;
         private readonly IMapper _mapper;
         private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
         public AccountController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager, ITokenService tokenService, IMapper mapper)
         {
+            _signInManager = signInManager;
             _userManager = userManager;
             _mapper = mapper;
             _tokenService = tokenService;
@@ -58,6 +60,10 @@ namespace API.Controllers
                 return Unauthorized("Invalid Username");
             }
 
+            var result = await _signInManager.CheckPasswordSignInAsync(user, loginDto.Password, false);
+
+            if(!result.Succeeded) return Unauthorized();
+
             return new UserDto{
                 Username = user.UserName,
                 Token = _tokenService.CreateToken(user),
@@ -70,7 +76,7 @@ namespace API.Controllers
 
         private async Task<bool> UserExists(string username)
         {
-            return await _context.Users.AnyAsync(x => x.UserName == username.ToLower());
+            return await _userManager.Users.AnyAsync(x => x.UserName == username.ToLower());
         }
     }
 }
