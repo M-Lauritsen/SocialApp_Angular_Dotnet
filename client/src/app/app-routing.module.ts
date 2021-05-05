b203545a52234bcd08a3b3057a46478af6ec1dd2
Michael Lauritsen <mla@complyto.com>
2021-05-05 07:13:40 +0200

    admin guard done

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -10,6 +10,7 @@ import { MemberDetailComponent } from './members/member-detail/member-detail.com
 import { MemberEditComponent } from './members/member-edit/member-edit.component';
 import { MemberListComponent } from './members/member-list/member-list.component';
 import { MessagesComponent } from './messages/messages.component';
+import { AdminGuard } from './_guards/admin.guard';
 import { AuthGuard } from './_guards/auth.guard';
 import { PreventUnsavedChangesGuard } from './_guards/prevent-unsaved-changes.guard';
 import { MemberDetailedResolver } from './_resolvers/member-detailed.resolver';
@@ -49,6 +50,7 @@ const routes: Routes = [
       {
         path: 'admin',
         component: AdminPanelComponent,
+        canActivate: [AdminGuard],
       },
     ],
   },
