67da6acec2fd4d208d31f112d7b9bcf18781519d
Michael Lauritsen <mla@complyto.com>
2021-05-10 08:06:35 +0200

    signalr tracker

--- API/SignalR/PresenceTracker.cs
+++ API/SignalR/PresenceTracker.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace API.SignalR
 {
@@ -6,5 +7,16 @@ namespace API.SignalR
     {
         private static readonly Dictionary<string, List<string>> OnlineUser = 
             new Dictionary<string, List<string>>();
+
+        public Task UserConnected(string username, string connectionId)
+        {
+            lock (OnlineUser)
+            {
+                if(OnlineUser.ContainsKey(username))
+                {
+                    OnlineUser[username].Add(connectionId);
+                }
+            }
+        }
     }
 }
