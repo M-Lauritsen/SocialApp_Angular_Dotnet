baf6e7f551c0f5353476afb0045c4b67a4ad8fc6
Michael Lauritsen <mla@complyto.com>
2021-05-11 07:34:12 +0200

    online presence done

--- API/SignalR/PresenceTracker.cs
+++ API/SignalR/PresenceTracker.cs
@@ -1,22 +1,56 @@
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace API.SignalR
 {
     public class PresenceTracker
     {
-        private static readonly Dictionary<string, List<string>> OnlineUser = 
+        private static readonly Dictionary<string, List<string>> OnlineUsers = 
             new Dictionary<string, List<string>>();
 
         public Task UserConnected(string username, string connectionId)
         {
-            lock (OnlineUser)
+            lock (OnlineUsers)
             {
-                if(OnlineUser.ContainsKey(username))
+                if(OnlineUsers.ContainsKey(username))
                 {
-                    OnlineUser[username].Add(connectionId);
+                    OnlineUsers[username].Add(connectionId);
+                }
+                else
+                {
+                    OnlineUsers.Add(username, new List<string>{connectionId});
                 }
             }
+
+            return Task.CompletedTask;
+        }
+
+        public Task UserDisconnected(string username, string connectionId)
+        {
+            lock(OnlineUsers)
+            {
+                if(!OnlineUsers.ContainsKey(username)) return Task.CompletedTask;
+
+                OnlineUsers[username].Remove(connectionId);
+                if(OnlineUsers[username].Count == 0)
+                {
+                    OnlineUsers.Remove(username);
+                }
+            }
+
+            return Task.CompletedTask;
+        }
+
+        public Task<string[]> GetOnlineUsers()
+        {
+            string[] onlineUsers;
+            lock(OnlineUsers)
+            {
+                onlineUsers = OnlineUsers.OrderBy(k => k.Key).Select(k => k.Key).ToArray();
+            }
+
+            return Task.FromResult(onlineUsers);
         }
     }
 }
