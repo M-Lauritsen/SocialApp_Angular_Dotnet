05dcf1558f1bc991488a1b8700565b5631539024
Michael Lauritsen <mla@complyto.com>
2021-05-12 08:18:41 +0200

    signalR added

--- client/src/app/members/member-detail/member-detail.component.ts
+++ client/src/app/members/member-detail/member-detail.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { TabDirective, TabsetComponent } from 'ngx-bootstrap/tabs';
 import { take } from 'rxjs/operators';
 
@@ -27,11 +27,13 @@ export class MemberDetailComponent implements OnInit, OnDestroy {
     public presence: PresenceService,
     private route: ActivatedRoute,
     private messageService: MessageService,
-    private accountService: AccountService
+    private accountService: AccountService,
+    private router: Router
   ) {
     this.accountService.currentUser$
       .pipe(take(1))
       .subscribe((user) => (this.user = user));
+    this.router.routeReuseStrategy.shouldReuseRoute = () => false;
   }
 
   ngOnInit(): void {
